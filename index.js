b562411f119b34646f0353edac7b2b31ef22b2eb
Hieu <xuanhieu0012@gmail.com>
2021-08-09 21:13:58 -0500

    Complete

---
+++ index.js
@@ -0,0 +1,17 @@
+// call back function
+function receivesAFunction(callback){
+    return callback();
+}
+ 
+
+function returnsANamedFunction(){
+    return function name(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
